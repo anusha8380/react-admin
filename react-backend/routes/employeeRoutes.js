d9ab099d7b659f3479de508cf157c45b72d7d7a7
anusha8380 <anusha.m@happiestminds.com>
2023-08-10 09:46:44 +0530

    route added for individual employee

--- react-backend/routes/employeeRoutes.js
+++ react-backend/routes/employeeRoutes.js
@@ -14,6 +14,7 @@ employee_routes.post("/register",employeeController.registerUser);
 employee_routes.post("/login",employeeController.loginUser);
 employee_routes.post("/create",employeeController.createUser);
 employee_routes.post("/user/:user_id",employeeController.updateUser);
+employee_routes.get("/user/:user_email",employeeController.getUser);
 employee_routes.delete("/delete/:user_id",employeeController.deleteUser);
 
 module.exports = employee_routes;
